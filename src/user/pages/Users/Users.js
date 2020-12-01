898ee645122cece5018b49a9e3ced9bbc626d7e3
vradu007 <radu.voin@gmail.com>
2020-12-01 11:39:05 +0200

    added card ui component

--- src/user/pages/Users/Users.js
+++ src/user/pages/Users/Users.js
@@ -1,21 +1,26 @@
 import React from 'react';
 
-import UsersList from '../../components/UsersList/UsersList'
+import UsersList from '../../components/UsersList/UsersList';
 
 const Users = () => {
-    const USERS = [{
-        id: 'u1',
-        name: 'user 1',
-        image: 'https://i2-prod.liverpool.com/incoming/article18453566.ece/ALTERNATES/s615/0_GettyImages-1210976964.jpg',
-        places: 3
-    },{
-        id: 'u2',
-        name: 'user 2',
-        image: 'https://imgresizer.eurosport.com/unsafe/1200x0/filters:format(jpeg)/origin-imgresizer.eurosport.com/2020/02/15/2775728-57336575-2560-1440.jpg',
-        places: 4
-    }];
+	const USERS = [
+		{
+			id: 'u1',
+			name: 'Mohamed Salah',
+			image:
+				'https://i2-prod.liverpool.com/incoming/article18453566.ece/ALTERNATES/s615/0_GettyImages-1210976964.jpg',
+			places: 3,
+		},
+		{
+			id: 'u2',
+			name: 'Sadio Mane',
+			image:
+				'https://imgresizer.eurosport.com/unsafe/1200x0/filters:format(jpeg)/origin-imgresizer.eurosport.com/2020/02/15/2775728-57336575-2560-1440.jpg',
+			places: 4,
+		},
+	];
 
-    return <UsersList items={USERS}/>;
-}
- 
+	return <UsersList items={USERS} />;
+};
+
 export default Users;
