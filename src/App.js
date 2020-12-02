e53ec3d74ddf944d8c1ed298ba977bb18259992b
vradu007 <radu.voin@gmail.com>
2020-12-03 00:26:22 +0200

    created PlaceList and PlaceItem components and added proper routing and params identification

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {
 import NewPlace from './places/pages/NewPlace/NewPlace';
 import MainNavigation from './shared/components/Navigation/MainNavigation/MainNavigation';
 import Users from './user/pages/Users/Users';
+import UserPlaces from './places/pages/UserPlaces/UserPlaces';
 
 const App = () => {
 	return (
@@ -17,6 +18,7 @@ const App = () => {
 			<main>
 				<Switch>
 					<Route path="/" exact component={Users} />
+					<Route path="/:userId/places" exact component={UserPlaces} />
 					<Route path="/places/new" component={NewPlace} />
 					<Redirect to="/" />
 				</Switch>
