063718beffd6b7961a6e41141f133615866d6787
vradu007 <radu.voin@gmail.com>
2020-12-07 09:51:13 +0200

    implemented view on map functionality

--- src/places/components/PlaceList/PlaceItem/PlaceItem.js
+++ src/places/components/PlaceList/PlaceItem/PlaceItem.js
@@ -4,6 +4,7 @@ import Button from '../../../../shared/components/FormElements/Button';
 import Card from '../../../../shared/components/UIElements/Card/Card';
 import Modal from '../../../../shared/components/UIElements/Modal/Modal';
 import './PlaceItem.css';
+import Map from './../../../../shared/components/UIElements/Map/Map'
 
 const PlaceItem = (props) => {
 	const [showMap, setShowMap] = useState(false);
@@ -23,7 +24,7 @@ const PlaceItem = (props) => {
 				footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
 			>
 				<div className="map-container">
-					<h2>THE MAP!</h2>
+					<Map center={props.coordinates} zoom={16} />
 				</div>
 			</Modal>
 			<li className="place-item">
